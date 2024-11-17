0452a8c04f1f21321d14f8b3b4a65ad9b83b4989
Aggelosk <akermezo@yahoo.com>
2024-11-17 15:12:46 +0200

    backend routes controllers

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import User from "../models/userModel.js";
+import { registerUser, loginUser } from "../comtrollers/userController.js"
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-const PORT = process.env.PORT || 5001;
+dotenv.config();
+connectDB();
+
+const port = process.env.PORT || 5001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
