9095ce63ad1df80d7d79a7e917b23ca5c3d0dcda
Aggelosk <akermezo@yahoo.com>
2024-11-17 14:44:48 +0200

    packages instalations,connecting front and back

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
