9bde7bb790628a243fee8c960b9817ce60d89494
Aggelosk <akermezo@yahoo.com>
2024-11-17 16:54:36 +0200

    Header and index forntend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import HomeScreen from "./Screens/HomeScreen";
+import CartScreen from "./Screens/CartScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomeScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+    </Route>
+  )
 );
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
