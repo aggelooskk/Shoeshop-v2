3bebf72e9e844e741794591f7e02285807bf9bd5
Aggelosk <akermezo@yahoo.com>
2024-11-24 18:34:06 +0200

    carousel redesign

--- frontend/src/Components/ProductCarousel.jsx
+++ frontend/src/Components/ProductCarousel.jsx
@@ -16,19 +16,19 @@ const ProductCarousel = () => {
     <>
       <Carousel
         slide={true}
-        className="py-1 my-5 justify-content-center align-items-center"
+        className="py-1 my-5"
       >
         {products.map((product, index) => (
           <Carousel.Item key={index}>
             <Row className="d-flex justify-content-center align-items-center">
-              <Col md={6}>
+              <Col>
                 <Image className="carousel-image"
                   src={product.image}
                   alt={product.name}
                   fluid
                 />
               </Col>
-              <Col md={6} className="text-start">
+              <Col className="text-start">
                 <h3>{product.name}</h3>
                 <p>{product.description}</p>
               </Col>
